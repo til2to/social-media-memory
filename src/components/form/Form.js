c425c32ff4874abcf50e69c91c37eb5bb869cd15
Owusu Dennis Prah <45304000+til2to@users.noreply.github.com>
2023-05-10 07:46:05 +0000

    Update Form.js

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -29,7 +29,7 @@ const Form = ({ currentId, setCurrentId }) => {
   let user = null;
 
   if (encryptedPayload) {
-    const bytes = CryptoJS.AES.decrypt(encryptedPayload, "my secret key with spaces and hashes#");
+    const bytes = CryptoJS.AES.decrypt(encryptedPayload, "your secret key");
     if (bytes.toString().length > 0) {
       const decryptedPayload = bytes.toString(CryptoJS.enc.Utf8);
       user = JSON.parse(decryptedPayload);
